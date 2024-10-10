d53666145ec35e5f71ad0a7a245aa4b968ac36bc
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2024-10-10 16:31:51 +0300

    Update settings and configuration for static and media files
    - Modified .gitignore to remove unnecessary static directory entry, ensuring cleaner repository management.
    - Updated core/settings.py:
      - Changed STATIC_URL to '/static/' for better URL handling.
      - Added STATIC_ROOT for production use, directing collectstatic output to 'staticfiles' directory.
      - Defined STATICFILES_DIRS to specify additional directories for static files.
      - Configured MEDIA_URL and MEDIA_ROOT for handling user-uploaded files.
    
    - Enhanced core/urls.py to serve static and media files during development:
      - Imported necessary settings and added logic to serve static and media files based on DEBUG mode.
    
    - Updated requirements.txt to include Pillow, a library for image processing, to support media file handling.

--- core/settings.py
+++ core/settings.py
@@ -130,7 +130,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# This is where collectstatic will store the static files in production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional locations the staticfiles app will traverse
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Media files (Uploaded by users)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
